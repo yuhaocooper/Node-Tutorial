1f3dc28379bca9170ee47851603801f9945b3bea
yuhao <tanyuhao@hotmail.com>
2022-02-06 16:25:16 +0800

    Section 4.17

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,59 @@
 const notesExport = require('./notes')
 const chalk = require('chalk')
+const yargs = require('yargs')
 
-const msg = notesExport()
-console.log(msg)
+// Customize yargs version
+yargs.version('1.1.0')
 
-const chalkMsg = chalk.red('Hello', chalk.bold.bgYellow('Hell'))
-console.log(chalkMsg)
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function(argv){
+        console.log('Title: ' + argv.title)
+        console.log('Body: ' + argv.body)
+    }
+})
+
+// Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function(){
+        console.log('Removing the note')
+    }
+})
+
+// Create list command
+yargs.command({
+    command: 'list',
+    describe: 'List a note',
+    handler: function(){
+        console.log('Listing the note')
+    }
+})
+
+// Create read command
+yargs.command({
+    command: 'read',
+    describe: 'Reading a note',
+    handler: function(){
+        console.log('Reading the note')
+    }
+})
+
+//  add, remove, read, list
+
+yargs.parse()
