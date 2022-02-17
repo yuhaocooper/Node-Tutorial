d4ad6b4c0a7d3cd8954eebceb9f26ea3bc922a72
yuhao <tanyuhao@hotmail.com>
2022-02-17 10:30:23 +0800

    6.33

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,7 @@
-console.log('Starting')
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 Second Timer')
-}, 2000)
+const url = 'http://api.weatherstack.com/current?access_key=d3e0712d4efe15438b81f277f73d3cd2&query=37.8267,-122.4233&units=f'
 
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0)
-
-console.log('Stopping')
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees out. There is ' + response.body.current.precip + '% chance of rain.')
+})
